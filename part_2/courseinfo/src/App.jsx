c3a0e5edc77e14c6557621ab156fd0b8fab43421
Francisco Fornasier Milani <65914041+FranciscoMilani@users.noreply.github.com>
2025-03-22 15:50:20 -0300

    2.4 courseinfo

--- part_2/courseinfo/src/App.jsx
+++ part_2/courseinfo/src/App.jsx
@@ -39,32 +39,59 @@ const Part = (props) => {
     );
 };
 
-const Total = (props) => <p>Number of exercises {props.total}</p>;
+const Total = (props) => (
+    <p style={{ fontWeight: "bold" }}>
+        Total of exercises {props.total} exercises
+    </p>
+);
 
 const App = () => {
-    const course = {
-        id: 1,
-        name: "Half Stack application development",
-        parts: [
-            {
-                name: "Fundamentals of React",
-                exercises: 10,
-                id: 1,
-            },
-            {
-                name: "Using props to pass data",
-                exercises: 7,
-                id: 2,
-            },
-            {
-                name: "State of a component",
-                exercises: 14,
-                id: 3,
-            },
-        ],
-    };
+    const courses = [
+        {
+            name: "Half Stack application development",
+            id: 1,
+            parts: [
+                {
+                    name: "Fundamentals of React",
+                    exercises: 10,
+                    id: 1,
+                },
+                {
+                    name: "Using props to pass data",
+                    exercises: 7,
+                    id: 2,
+                },
+                {
+                    name: "State of a component",
+                    exercises: 14,
+                    id: 3,
+                },
+                {
+                    name: "Redux",
+                    exercises: 11,
+                    id: 4,
+                },
+            ],
+        },
+        {
+            name: "Node.js",
+            id: 2,
+            parts: [
+                {
+                    name: "Routing",
+                    exercises: 3,
+                    id: 1,
+                },
+                {
+                    name: "Middlewares",
+                    exercises: 7,
+                    id: 2,
+                },
+            ],
+        },
+    ];
 
-    return <Course course={course} />;
+    return courses.map((x) => <Course course={x} />);
 };
 
 export default App;
