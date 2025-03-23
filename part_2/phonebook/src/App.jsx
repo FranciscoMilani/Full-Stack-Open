4d38f0bdc54c7d5e749a3aaffeb2b98b01999e9f
Francisco Fornasier Milani <65914041+FranciscoMilani@users.noreply.github.com>
2025-03-23 19:53:12 -0300

    2.6 phonebook

---
+++ part_2/phonebook/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
+
+const App = () => {
+    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+    const [newName, setNewName] = useState("");
+
+    const addNumberHandler = (event) => {
+        event.preventDefault();
+        if (!newName) return;
+
+        const newNameObj = {
+            name: newName,
+        };
+        setPersons(persons.concat(newNameObj));
+        setNewName("");
+    };
+
+    const setNewNameHandler = (event) => {
+        setNewName(event.target.value);
+    };
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addNumberHandler}>
+                <div>
+                    name: <input value={newName} onChange={setNewNameHandler} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {persons.map((x) => (
+                <p key={nanoid()}>{x.name}</p>
+            ))}
+        </div>
+    );
+};
+
+export default App;
