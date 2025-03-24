b44d32ba239334aba60ea0c006625b57fae5fa13
Francisco Fornasier Milani <65914041+FranciscoMilani@users.noreply.github.com>
2025-03-23 22:45:20 -0300

    2.8 phonebook

--- part_2/phonebook/src/App.jsx
+++ part_2/phonebook/src/App.jsx
@@ -2,8 +2,11 @@ import { useState } from "react";
 import { nanoid } from "nanoid";
 
 const App = () => {
-    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+    const [persons, setPersons] = useState([
+        { name: "Arto Hellas", phone: "040-1234567" },
+    ]);
     const [newName, setNewName] = useState("");
+    const [newPhone, setNewPhone] = useState("");
 
     const addNumberHandler = (event) => {
         event.preventDefault();
@@ -16,13 +19,20 @@ const App = () => {
 
         const newNameObj = {
             name: newName,
+            phone: newPhone,
         };
+
         setPersons(persons.concat(newNameObj));
         setNewName("");
+        setNewPhone("");
     };
 
     const setNewNameHandler = (event) => {
         setNewName(event.target.value);
+    };
+
+    const setNewPhoneHandler = (event) => {
+        setNewPhone(event.target.value);
     };
 
     return (
@@ -30,15 +40,35 @@ const App = () => {
             <h2>Phonebook</h2>
             <form onSubmit={addNumberHandler}>
                 <div>
-                    name: <input value={newName} onChange={setNewNameHandler} />
+                    <div>
+                        <label for="name">name: </label>
+                        <input
+                            id="name"
+                            value={newName}
+                            onChange={setNewNameHandler}
+                        />
+                    </div>
+
+                    <div>
+                        <label for="phone">phone: </label>
+                        <input
+                            id="phone"
+                            value={newPhone}
+                            onChange={setNewPhoneHandler}
+                        />
+                    </div>
                 </div>
                 <div>
                     <button type="submit">add</button>
                 </div>
             </form>
+
             <h2>Numbers</h2>
+
             {persons.map((x) => (
-                <p key={nanoid()}>{x.name}</p>
+                <p key={nanoid()}>
+                    {x.name} {x.phone}
+                </p>
             ))}
         </div>
     );
